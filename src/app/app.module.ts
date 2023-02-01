fd1384f2f2a23b53eab82c07cce66b2e54cffc95
ballatamas98 <36437827+ballatamas98@users.noreply.github.com>
2023-02-01 12:08:22 +0100

    add game board + design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './components/game/game.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { GameComponent } from './components/game/game.component';
     GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
 
+  protected board: number[][] = this.getTwoDimensionArrayFilledZeros() ;
+
+  constructor(){
+    
+   
+  }
+
+  private getTwoDimensionArrayFilledZeros(): number[][]{
+    let arr = new Array<Array<number>>;
+    for (let i = 0; i < 3; i++) {
+      arr.push(new Array<number>(3).fill(0))
+    }
+    return arr;
+  }
 }
